64b92755b1f6721db7eb92ac1878cc16e584e372
Xu Gang <1497312978@qq.com>
2024-11-06 22:15:16 +0800

    删除不需要的文件

--- python作业/python作业6/水仙花.py
+++
@@ -1,9 +0,0 @@
-sum=0
-for i in range(100,999):
-    a=i%10
-    b=(i//10)%10
-    c=(i//100)%10
-    if(i==a**3+b**3+c**3):
-        print(i,",",end=(""))
-        sum=sum+1
-print("水仙花数共有%d个"%sum)
--- python作业/python作业9/2.py
+++
@@ -1,8 +0,0 @@
-time=11
-num=26625
-print("过了{}天有{}个细胞".format(time,num))
-
-for i in range(11):
-    time=time-1
-    num=(num-1)/2+1
-    print("过了{}天有{}个细胞".format(time,num))
